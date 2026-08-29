1b769087f20f33b88c9dffb23eb2b9eaa951cd64
Aspirant <aspirant@syllabus3d.app>
2026-08-29 21:04:47 +0530

    fix(layout): lock sidebar as fixed top-0 left-0 and offset main workspace with md:pl-64 so sidebar remains frozen during scroll

--- src/App.tsx
+++ src/App.tsx
@@ -285,7 +285,7 @@ export const App: React.FC = () => {
       />
 
       {/* Main Workspace Frame */}
-      <div className="flex-1 flex flex-col min-w-0">
+      <div className="flex-1 flex flex-col min-w-0 md:pl-64">
         <Header
           onOpenSearch={() => {
             setIsSearchOpen(true);
